5f85ea64469674b73059f431055d3ed33575b582
emmafewer <emmafewer20@gmail.com>
2021-02-10 11:34:13 -0800

    done

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,11 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => 
+      <div className="actor">{director.name} 
+        <ul>{director.movies}</ul>
+      </div>)}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,11 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => 
+      <div className="actor">{movie.title} {movie.time} {movie.metascore} 
+        <ul>{movie.genres}</ul>
+      </div>)}
     </div>
   );
 };
